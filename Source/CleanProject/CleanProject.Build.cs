ea08bb671e4c1dc1f3d34d4d240cc0c23f778e7a
Alexandru Oprea <oalexprea@yahoo.com>
2021-03-24 15:27:50 +0200

    build.cs file cleaned up

--- Source/CleanProject/CleanProject.Build.cs
+++ Source/CleanProject/CleanProject.Build.cs
@@ -6,66 +6,19 @@ public class CleanProject : ModuleRules
 {
 	public CleanProject(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-
-		PublicIncludePaths.AddRange(
-			new string[] {
-                "Developer/AssetTools/Private",
-				// ... add public include paths required here ...
-			}
-            );
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-            }
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-                "AssetTools",
-                "Core",
-				"AssetManagerEditor",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-
-		PrivateDependencyModuleNames.AddRange(new string[] {
-					"AssetManagerEditor"
-			});
-
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
-                "AssetTools",
-                "ContentBrowser",
+				"AssetManagerEditor",
                 "Core",
                 "CoreUObject",
-                "DesktopWidgets",
                 "EditorStyle",
                 "Engine",
                 "InputCore",
-                "Projects",
+                "MainFrame",
                 "Slate",
                 "SlateCore",
                 "UnrealEd",
-                "Projects",
-                "MainFrame",
-				//"ToolMenus",
-
-				// ... add private dependencies that you statically link with here ...	
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+			} );
 	}
 }
