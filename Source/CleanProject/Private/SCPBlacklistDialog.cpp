238aa74b316f2659c1b3e77a4034850185ad611b
refik yaşaroğlu <refikyasaroglu@gmail.com>
2022-01-11 13:44:02 +0000

    Update SCPBlacklistDialog.cpp

--- Source/CleanProject/Private/SCPBlacklistDialog.cpp
+++ Source/CleanProject/Private/SCPBlacklistDialog.cpp
@@ -4,11 +4,11 @@
 
 #include "CPOperations.h"
 #include "CPSettings.h"
-#include "Widgets/Layout/SUniformGridPanel.h"
 
-#include "Widgets\Input\SCheckBox.h"
-#include "Widgets\Input\SButton.h"
-#include "Widgets\Layout\SSpacer.h"
+#include "Widgets/Layout/SUniformGridPanel.h"
+#include "Widgets/Input/SCheckBox.h"
+#include "Widgets/Input/SButton.h"
+#include "Widgets/Layout/SSpacer.h"
 #include "Editor.h"
 
 #define LOCTEXT_NAMESPACE "CleanProject"
