b4d88d2f0988aa308eab85c6b3b8168a77e6cdb8
pasotee <oprea_alexandrum@yahoo.com>
2022-12-06 13:00:08 +0200

    fixed wrong branch using force

--- Source/CleanProject/Private/CleanProjectCommandlet.cpp
+++ Source/CleanProject/Private/CleanProjectCommandlet.cpp
@@ -7,17 +7,17 @@
 
 int32 UCleanProjectCommandlet::Main(const FString& Params)
 {
-	UE_LOG(LogCleanProject, Display, TEXT("Running Clean Project Commandlet with: %s."), *Params);
+	UE_LOG(LogCleanProject, Display, TEXT("Running Clean Project Commandlet with: %s"), *Params);
 
 	const bool bLogUnused = FParse::Param(*Params, TEXT("LogUnused"));
 	const bool bDelete = FParse::Param(*Params, TEXT("Delete"));
 	const bool bForce = FParse::Param(*Params, TEXT("Force"));
 
-	UE_LOG(LogCleanProject, Display, TEXT("Parsed parameters LogUnused: %s Delete: %s Force: %s"), *LexToString(bLogUnused),
+	UE_LOG(LogCleanProject, Display, TEXT("Parsed parameters: { LogUnused: %s Delete: %s Force: %s }"), *LexToString(bLogUnused),
 		*LexToString(bDelete), *LexToString(bForce));
 
 	TArray<FAssetData> UnusedAssets = UCPOperationsSubsystem::Get()->GetAllUnusedAssets(EScanType::Fast);
-	UE_LOG(LogCleanProject, Display, TEXT("Found: %s unused assets."), *LexToString(UnusedAssets.Num()));
+	UE_LOG(LogCleanProject, Display, TEXT("Found: %s unused assets"), *LexToString(UnusedAssets.Num()));
 	if (bLogUnused)
 	{
 		for (const FAssetData& AssetData : UnusedAssets)
@@ -28,7 +28,7 @@ int32 UCleanProjectCommandlet::Main(const FString& Params)
 
 	if (UnusedAssets.Num() == 0)
 	{
-		UE_LOG(LogCleanProject, Display, TEXT("No unused assets found."));
+		UE_LOG(LogCleanProject, Display, TEXT("No unused assets found"));
 	}
 	else if (bDelete)
 	{
@@ -48,14 +48,14 @@ void UCleanProjectCommandlet::DeleteAssets(const TArray<FAssetData>& AssetsToDel
 
 	if (bForce)
 	{
-		UE_LOG(LogCleanProject, Display, TEXT("Deleting assets"));
-		const int32 NumDeleted = ObjectTools::DeleteObjects(ObjectsToDelete, false);
+		UE_LOG(LogCleanProject, Display, TEXT("Deleting assets forcefully"));
+		const int32 NumDeleted = ObjectTools::ForceDeleteObjects(ObjectsToDelete, false);
 		UE_LOG(LogCleanProject, Display, TEXT("Deleted: %s assets"), *LexToString(NumDeleted));
 	}
 	else
 	{
-		UE_LOG(LogCleanProject, Display, TEXT("Deleting assets forcefully"));
-		const int32 NumDeleted = ObjectTools::ForceDeleteObjects(ObjectsToDelete, false);
+		UE_LOG(LogCleanProject, Display, TEXT("Deleting assets"));
+		const int32 NumDeleted = ObjectTools::DeleteObjects(ObjectsToDelete, false);
 		UE_LOG(LogCleanProject, Display, TEXT("Deleted: %s assets"), *LexToString(NumDeleted));
 	}
 }
